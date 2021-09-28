962e8c240f91702753fc12fed6f836ba1d7dd698
Margus Reintam <margus.reintam@milrem.com>
2021-09-28 21:59:04 +0300

    added google auth to django

--- sivako_django/sivako_django/settings.py
+++ sivako_django/sivako_django/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--5eka2+6hsbpia0$90@5glgy2w!-ijtsgw(*&@te)0z3_o0hmm'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
